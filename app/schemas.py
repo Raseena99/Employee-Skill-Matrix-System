9375571f2b88dedf461073d7ba9b591d75fafb4b
Raseena99 <raseenacp24@gmail.com>
2026-05-22 02:24:54 +0530

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,24 +1,29 @@
 from pydantic import BaseModel
+
 class EmployeeCreate(BaseModel):
-name: str
-email: str
-department: str
-designation: str
-experience: float
-skills: list[str]
+    name: str
+    email: str
+    department: str
+    designation: str
+    experience: float
+    skills: list[str]
+    
 class EmployeeResponse(BaseModel):
-id: int
-name: str
-email: str
-department: str
-designation: str
-experience: float
-skills: list[str]
-class Config:
-orm_mode = True
+    id: int
+    name: str
+    email: str
+    department: str
+    designation: str
+    experience: float
+    skills: list[str]
+    
+    class Config:
+        orm_mode = True
+
 class UserCreate(BaseModel):
-username: str
-password: str
+    username: str
+    password: str
+
 class Token(BaseModel):
-access_token: str
-token_type: str
+    access_token: str
+    token_type: str
