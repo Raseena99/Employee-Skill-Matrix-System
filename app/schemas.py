cf2aaf6470963b9fbf505a592573daf594359a7d
Raseena99 <raseenacp24@gmail.com>
2026-05-21 18:10:42 +0530

    Create schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+class EmployeeCreate(BaseModel):
+name: str
+email: str
+department: str
+designation: str
+experience: float
+skills: list[str]
+class EmployeeResponse(BaseModel):
+id: int
+name: str
+email: str
+department: str
+designation: str
+experience: float
+skills: list[str]
+class Config:
+orm_mode = True
+class UserCreate(BaseModel):
+username: str
+password: str
+class Token(BaseModel):
+access_token: str
+token_type: str
