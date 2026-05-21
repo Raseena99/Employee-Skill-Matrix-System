bbe65db11d03791deba261b8ede08d9f9ac25632
Raseena99 <raseenacp24@gmail.com>
2026-05-22 02:38:27 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
+
 from .database import Base
 from .database import engine
 from .routes import router
+
 Base.metadata.create_all(bind=engine)
+
 app = FastAPI(
-title="Employee Skill Matrix System",
-version="1.0.0",
-description="Enterprise skill tracking platform"
+    title="Employee Skill Matrix System",
+    version="1.0.0",
+    description="Enterprise skill tracking platform"
 )
+
 app.include_router(router)
+
 @app.get("/")
 def home():
-return {
-"message": "Employee Skill Matrix API Running"
-}
+    return {
+        "message": "Employee Skill Matrix API Running"
+    }
