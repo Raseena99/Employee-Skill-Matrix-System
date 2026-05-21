fa7fda7f7eb01fc63b27cb552993a79e0ae9b396
Raseena99 <raseenacp24@gmail.com>
2026-05-22 02:23:14 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,18 +2,23 @@ from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy import Float
+
 from .database import Base
+
 class Employee(Base):
-__tablename__ = "employees"
-id = Column(Integer, primary_key=True, index=True)
-name = Column(String, nullable=False)
-email = Column(String, unique=True)
-department = Column(String)
-designation = Column(String)
-experience = Column(Float)
-skills = Column(String)
+    __tablename__ = "employees"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True)
+    department = Column(String)
+    designation = Column(String)
+    experience = Column(Float)
+    skills = Column(String)
+
 class User(Base):
-__tablename__ = "users"
-id = Column(Integer, primary_key=True, index=True)
-username = Column(String, unique=True)
-hashed_password = Column(String)
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    hashed_password = Column(String)
